1706bb80b90df0e94bbef71a408da7cdff98dfe5
Yak Jun Xiang <snowypowers@gmail.com>
2017-07-04 19:36:25 +0800

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/picnic/releases/picnic.min.css'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
