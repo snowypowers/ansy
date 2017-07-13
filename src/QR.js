a4ee827e24d1aea34b26ddeef1d5fca8ef2f513e
Yak Jun Xiang <snowypowers@gmail.com>
2017-07-13 15:52:37 +0800

    Implement QR double click for str

--- src/QR.js
+++ src/QR.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QRCode from 'qrcode'
+import './QR.css'
 
 const makeQR = (cvs, str) => {
   QRCode.toCanvas(cvs, str, { version: 5 }, (err) => {
@@ -8,13 +9,33 @@ const makeQR = (cvs, str) => {
 }
 
 class QR extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showQR: true
+    }
+    this.toggleQR = this.toggleQR.bind(this)
+  }
+
+  toggleQR() {
+    if (this.state.showQR) {
+      this.setState({ showQR: false })
+    } else {
+      this.setState({ showQR: true })
+    }
+  }
+
   componentDidMount() {
     makeQR(this.cvs, this.props.str)
   }
+
   render() {
     return (
       <div className="qr-block">
-        <canvas className="qr" ref={(i) => this.cvs = i}></canvas>
+        <div className="qr" onDoubleClick={this.toggleQR}>
+          <canvas className={this.state.showQR ? 'showQR' : 'hideQR'} ref={(i) => this.cvs = i} ></canvas>
+          <span className={(this.state.showQR ? 'hideQR' : 'showQR') + ' hexstr'} > {this.props.str}</span>
+        </div>
         <p>{this.props.name}</p>
       </div>
     )
