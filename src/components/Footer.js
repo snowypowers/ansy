fe0cbd9bf0cb5d52f3b353c7da728a33e1b87d8d
Yak Jun Xiang <snowypowers@gmail.com>
2017-08-17 23:13:13 +0800

    Build v1.4
    - Updated general UI
    - Updated Wallet UI - thinner with more text space
    - Ability to remove wallets
    - Updated printing layout - 4 Wallets per page, no splicing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <div id="foot">
-        <p>Made by snowypowers | v1.3.0</p>
+        <p>Made by snowypowers | v1.4.0</p>
         <p>Tips appreciated!</p>
         <div>NEO: AbUKT3KXcAMpLD1MsAMRNhgN3hqmzEqqVP</div>
         <div>ETH: 0x2E15cd6Ca9f8dcdCd08Ad62d901b6E6a815a36fd </div>
